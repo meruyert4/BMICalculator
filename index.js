ec89f92022525ebd4bb36a0bd316a9668346b087
meruyert4 <https://github.com/meruyert4>
2025-01-02 01:00:58 +0500

    bmi

--- index.js
+++ index.js
@@ -11,35 +11,34 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'webPage.html'));
 });
 
-app.post('/bmi', (req, res) => {
-    const weight = parseFloat(req.body.weight);
-    const height = parseFloat(req.body.height);
+app.post('/bmi', (req, res) =>{
+    const weight = parseFloat(req.body.weight)
+    const height = parseFloat(req.body.height)
+    const bmi = weight / (height/100) ** 2
+    let category
+    let color
 
-    if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
-        return res.status(400).send('Invalid input. Please provide valid weight and height.');
+    if (isNaN(weight) || isNaN(height) || weight <=0 || height <= 0){
+         color = 'red'
+        return res.status(400).send(`<p style = "color:${color}";>Invalid input. Try Again</p>`)
+    }
+    if (bmi < 18.5){
+        category = 'Underweight'
+        color = 'blue'
+    } else if(bmi >= 18.5 && bmi < 24.5){
+        category = 'Normal weight'
+        color = 'green'
+    } else if(bmi >= 25 && bmi < 29.9){
+        category = 'Overweight'
+        color='orange'
+    } else{
+        category = 'Obesity'
+        color = 'red'
     }
 
-    const bmi = weight / ((height / 100) ** 2);
-    let category;
-    let color;
+    res.send(`<p style="color:${color}";>Your bmi is ${bmi.toFixed(2)}. Your category is ${category}</p>`)
+})
 
-    if (bmi < 18.5) {
-        category = 'Underweight';
-        color = 'blue';
-    } else if (bmi >= 18.5 && bmi < 24.9) {
-        category = 'Normal weight';
-        color = 'green';
-    } else if (bmi >= 25 && bmi < 29.9) {
-        category = 'Overweight';
-        color = 'yellow';
-    } else {
-        category = 'Obesity';
-        color = 'red';
-    }
-
-    res.send(`<p style="color:${color};">Your BMI is ${bmi.toFixed(2)} and you are categorized as ${category}.</p>`);
-});
-
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-});
+app.listen(3000, () =>{
+    console.log('Server running on http://localhost:3000')
+})
